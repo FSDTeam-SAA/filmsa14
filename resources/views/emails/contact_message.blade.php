f492721fff1de36fe2f785a6e2e705a83701ca74
zahidulNahid <zahidulnahid7464@gmail.com>
2025-05-20 11:47:49 +0600

    inital

---
+++ resources/views/emails/contact_message.blade.php
@@ -0,0 +1,11 @@
+<h2>New Project Request</h2>
+
+<p><strong>First Name:</strong> {{ $contact->first_name }}</p>
+<p><strong>Last Name:</strong> {{ $contact->last_name }}</p>
+<p><strong>Phone Number:</strong> {{ $contact->phone_number }}</p>
+<p><strong>Company:</strong> {{ $contact->company_name }}</p>
+<p><strong>Email:</strong> {{ $contact->email_address }}</p>
+<p><strong>Comments/Questions:</strong> {{ $contact->comments }}</p>
+
+
+<p>Thank you!</p>
